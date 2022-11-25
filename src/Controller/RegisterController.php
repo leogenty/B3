3883c3eaaf05f6a9c02514bdc4d6bee372799e68
Jeremy Macadre <jeremymacadre@gmail.com>
2022-11-25 09:28:44 +0100

    MYL-8 Création Formulaire Inscription

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class RegisterController extends AbstractController
+{
+
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
+
+    #[Route('/inscription', name: 'app_register')]
+    public function index(Request $request, UserPasswordHasherInterface $hasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $user = $form->getData();
+
+                //vérifier si l'utilisateur n'existe pas en base de donnée
+
+
+                $password = $hasher->hashPassword($user,$user->getPassword());
+                $user->setPassword($password);
+
+
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+
+                return $this->redirectToRoute('front_home');
+
+
+        }
+
+        return $this->render('register/index.html.twig',[
+            'form' => $form->createView(),
+        ]);
+    }
+}
