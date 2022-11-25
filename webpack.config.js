bc9be679938bbd9f1a78145914625a863283268b
Jeremy Macadre <jeremymacadre@gmail.com>
2022-11-25 11:36:09 +0100

    MYL-10 Création-formulaire-connexion

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ Encore
     .addEntry('app_pages_class', './assets/javascripts/app/pages/class/index.js')
     .addEntry('app_pages_profile', './assets/javascripts/app/pages/profile/index.js')
     .addEntry('app_register', './assets/javascripts/security/pages/register/index.js')
+    .addEntry('app_login', './assets/javascripts/security/pages/login/index.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     // .enableStimulusBridge('./assets/controllers.json')
