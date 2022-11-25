63b4e960768ef9fc625a950cd56b8b7713aba7df
Léo Genty <l.genty@cs2i-bourgogne.com>
2022-11-25 15:42:08 +0100

    #comment add Lessons and Matter entity to Admin Dashboard

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Lessons;
+use App\Entity\Matter;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('admin.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('MyLearning')
+            ->setLocales(['fr']);
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
+        yield MenuItem::linkToCrud('Matter', 'fas fa-list', Matter::class);
+        yield MenuItem::linkToCrud('Lessons', 'fas fa-list', Lessons::class);
+    }
+}
