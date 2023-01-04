127f3a6118501c185bbbf863cc013d653d5cbef9
Jeremy Macadre <jeremymacadre@gmail.com>
2023-01-04 09:32:51 +0100

    Ajout de quelques petits éléments mineurs

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Lessons;
+use App\Entity\LessonSection;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
